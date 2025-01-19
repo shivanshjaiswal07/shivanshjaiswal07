89fd80dad0779e3b600f1c54f6ac0a2c3c962bc4
Shivansh Jaiswal <152159180+shivanshjaiswal07@users.noreply.github.com>
2025-01-19 14:23:59 +0530

    Exception handeling

--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,28 @@
-#include<iostream>
+#include <iostream>
+#include<stdexcept>
 using namespace std;
-class name{
-    public:
-    int a;
 
-    friend void display(a){
-        cout<<a;
+int main() {
+    int a,b,s;
+    cout<<"Enter a";
+    cin>>a;
+
+    cout<<"Enter b";
+    cin>>b;
+
+    try{
+        if(b==0){
+            throw b;
+            cout<<"This portion will be never be excetued";
+        }
+     s=a/b;
+     cout<<s;
     }
-};
 
-int main(){
-    name n;
-    n.a=10;
-    display(n);
+    catch(int x){
+        cout<<"last step";
+
+    }
+
+    return 0;
 }
